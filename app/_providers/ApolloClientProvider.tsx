2935cf454b922090627d261466f9044555d0cb08
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-01-31 17:49:13 +0530

    init: apollo client graphql

---
+++ app/_providers/ApolloClientProvider.tsx
@@ -0,0 +1,16 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import React from "react";
+
+const ApolloClientProvider = ({ children }: { children: React.ReactNode }) => {
+  const client = new ApolloClient({
+    uri: "https://hasura-template-production.up.railway.app/v1/graphql",
+    headers: {
+      "x-hasura-admin-secret": "iwillhack",
+    },
+    cache: new InMemoryCache(),
+  });
+
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+};
+
+export default ApolloClientProvider;
