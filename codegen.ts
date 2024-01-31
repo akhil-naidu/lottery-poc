783ee80082fe24cbcbc862267048804588959f02
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-01-31 22:09:03 +0530

    chore: added queries

---
+++ codegen.ts
@@ -0,0 +1,41 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: [
+    {
+      "https://hasura-template-production.up.railway.app/v1/graphql": {
+        headers: {
+          "x-hasura-admin-secret": "iwillhack",
+        },
+      },
+    },
+  ],
+  ignoreNoDocuments: true,
+  generates: {
+    "./generated/gql/": {
+      documents: "queries/**/*.{ts,tsx,graphql}",
+      overwrite: true,
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-apollo",
+        "typescript-graphql-request",
+      ],
+      config: {
+        withHooks: true,
+        withComponent: false,
+        withHOC: false,
+        withMutationFn: false,
+        preResolveTypes: true,
+        skipTypename: false,
+        avoidOptionals: true,
+      },
+    },
+    "./graphql.schema.json": {
+      plugins: ["introspection"],
+      config: { minify: true },
+    },
+  },
+};
+
+export default config;
